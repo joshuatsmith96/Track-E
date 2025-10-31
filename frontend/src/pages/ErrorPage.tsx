3cf873d4ddbbe3ce5c30b7c3166957281bc584a5
joshuatsmith96 <joshuatsmith96@gmail.com>
2025-10-31 15:25:32 -0400

    Fixed an issue with handling error pages.

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,9 +1,18 @@
 import Dashboard from "../components/DashboardMenu";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
+import { Navigate } from "react-router-dom";
 
 const ErrorPage = () => (
-  <Dashboard>
-    <h1>Error - Page not found</h1>
-  </Dashboard>
+  <>
+    <SignedIn>
+      <Dashboard>
+        <h1>Error - Page not found</h1>
+      </Dashboard>
+    </SignedIn>
+    <SignedOut>
+      <Navigate to="/login" />
+    </SignedOut>
+  </>
 );
 
 export default ErrorPage;
