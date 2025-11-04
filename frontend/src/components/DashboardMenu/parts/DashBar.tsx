c58a29b4264426941fe18a07ec83be913732c1ab
joshuatsmith96 <joshuatsmith96@gmail.com>
2025-11-04 13:39:23 -0500

    Created a new 'useScreenSize' hook for determining screen sizes

--- frontend/src/components/DashboardMenu/parts/DashBar.tsx
+++ frontend/src/components/DashboardMenu/parts/DashBar.tsx
@@ -1,17 +1,39 @@
-import { Stack } from "@mui/material";
+import { Stack, Box } from "@mui/material";
 import UserCircle from "./UserCircle";
 import { dashConfig } from "../dashConfig";
+import MenuIcon from "@mui/icons-material/Menu";
+import DashMenu from "./DashMenu";
 
 const DashBar = () => {
   return (
     <Stack
+      direction="row"
       sx={{
         backgroundColor: dashConfig.styles.menuBarBg || "white",
         width: "100%",
         height: "70px",
-        justifyContent: "center",
+        justifyContent: {
+          xs: "space-between",
+          sm: "flex-end",
+        },
+        alignItems: "center",
+        boxSizing: "border-box",
+        px: 2,
       }}
     >
+      <DashMenu visible={false} />
+      <Box
+        sx={{
+          display: {
+            sm: "none",
+          },
+          "&:hover": {
+            cursor: "pointer",
+          },
+        }}
+      >
+        <MenuIcon />
+      </Box>
       <UserCircle />
     </Stack>
   );
